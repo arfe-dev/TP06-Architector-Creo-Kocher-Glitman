59aedc203fc3ca7909d3a94145680c1d0d817cb3
FEDERICO MARTIN ARCHITECTOR <50416902@ort>
2026-09-03 08:32:45 -0300

    Views Inicial

--- Controllers/JuegoController.cs
+++ Controllers/JuegoController.cs
@@ -6,5 +6,28 @@ namespace PrimerProyecto.Controllers;
 
 public class JuegoController : Controller
 {
-    
+    public IActionResult Octavos()
+{
+    return View();
+}
+
+public IActionResult Cuartos()
+{
+    return View();
+}
+
+public IActionResult Semifinal()
+{
+    return View();
+}
+
+public IActionResult Final()
+{
+    return View();
+}
+
+public IActionResult Ganador()
+{
+    return View();
+}
 }
