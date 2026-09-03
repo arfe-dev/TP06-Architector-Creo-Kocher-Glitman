aa5ed9d55618c3f41c02adb56798863582d20de0
LORENZO CREO KOCHER <50049697@ort>
2026-09-03 08:53:12 -0300

    Model

--- Models/Partida.cs
+++
@@ -1,7 +0,0 @@
-namespace PrimerProyecto.Models;
-
-public class Partida{
-    public int Id { get; set; }
-    public string Descripcion { get; set; }
-    public string NombreParticipante { get; set; }
-}
--- Models/Sala.cs
+++
@@ -1,9 +0,0 @@
-namespace PrimerProyecto.Models;
-
-public class Sala{
-    public int Id { get; set; }
-    public string Descripcion { get; set; }
-    public string Pista { get; set; }
-    public string Nombre { get; set; }
-    public int IntentosRestantes { get; set; }
-}
